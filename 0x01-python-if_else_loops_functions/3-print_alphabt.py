40ca93f86862b57473e310d05567de887a4cd1cf
zoebelete <hiwotbelete04@gmail.com>
2022-05-31 07:32:54 -0700

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for a in range(ord("a"), ord("z") + 1):
-    if chr(a) is not "q" and chr(a) is not "e":
-        print("{:s}".format(chr(a)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
