375fa81338bca60062f5a281c60af44df34f86ef
zoebelete <hiwotbelete04@gmail.com>
2022-06-06 04:30:09 -0700

    data

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,15 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * main - check the code for alx School students.
+ * is_palindrome - checks if a linked list is a palindrome
  *
- * Return: Always 0.
+ * @head: the head address of the linked list
+ *
+ * Return: 1 if it's a palindrome, 0 else
  */
-int main(void)
-{
-	listint_t *head;
 
-	return (0);
+int is_palindrome(listint_t **head)
+{
+	listint_t *cur = *head;
+	int tab[2048], i = 0, j = 0;
+
+	if (*head)
+	{
+		while (cur)
+		{
+			tab[i] = cur->n;
+			cur = cur->next;
+			i++;
+		}
+
+		while (j < i / 2)
+		{
+			if (tab[j] == tab[i - j - 1])
+				j++;
+			else
+				return (0);
+		}
+	}
+	return (1);
 }
